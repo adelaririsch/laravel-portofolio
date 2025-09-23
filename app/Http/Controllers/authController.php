f871e777cf4ebcee9262d40b80719a985404bb3d
adelaririsch <adelaririsch@gmail.com>
2025-09-23 23:36:30 +0700

    first commit

---
+++ app/Http/Controllers/authController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+
+
+class authController extends Controller
+{
+    function index()
+    {
+        return view('auth.index');
+    }
+    function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    function callback()
+    {
+
+        $user = Socialite::driver('google')->user();
+        $id = $user->id;
+        $email = $user->email;
+        $name = $user->name;
+
+        $cek = User::where('email', $email)->count();
+
+        if($cek>0){
+            $user = User::updateOrCreate(
+            ['email'=> $email],
+                [
+                            'name' => $name,
+                            'google_id' => $id
+                        ]
+            );
+            Auth::login($user);
+            return redirect()->to('dashboard');
+        } else {
+            return redirect()->to('auth')->with('error', 'Akun yang anda masukkan tidak diizinkan untuk menggunakan halaman Admin');
+        }
+        
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->to('auth');
+    }
+        
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::redirect('home','dashboard');
+
+Route::get('/auth',[authController::class, "index"])->name('login')->middleware('guest');
+Route::get('/auth/redirect',[authController::class,"redirect"])->middleware('guest');
+
+//ini nampilkan halaman email kita 
+    // dua route ini untuk menjalannkan di web
+Route::get('/auth/callback', [authController::class,"callback"])->middleware('guest');
+Route::get('/auth/logout',[authController::class,"logout"]);
+Route::get('/dashboard', function (){
+    return 'Selamat Datang ' . Auth::user()->email . ' di halaman Dashboard';
+})->middleware('auth');
